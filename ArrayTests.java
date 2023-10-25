d797e31f7558772818bf6d1225cfaf4585f73592
rrbhushan <rrbhushan@ucsd.edu>
2023-10-25 16:46:12 -0700

    Fixed up ArrayExamples + modified ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlaceMult() {
+    int[] input1 = { 3 , 5 , 1 , 7 , 9 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 9 , 7 , 1 , 5 , 3 }, input1);
+	}
+
+  @Test
+  public void testReversedMult() {
+    int[] input1 = { 2 , 4 , 6};
+    assertArrayEquals(new int[]{ 6 , 4 , 2 }, ArrayExamples.reversed(input1));
   }
 }
